51b1714da254ec00181253d6af9f129ea3e6fa7d
Alif_Amarta <alifamarta23@gmail.com>
2023-11-24 22:49:32 +0700

    update

--- tambah.php
+++ tambah.php
@@ -2,8 +2,7 @@
 error_reporting(E_ALL);
 include_once 'koneksi.php';
 
-if (isset($_POST['submit']))
-{
+if (isset($_POST['submit'])) {
     $nama = $_POST['nama'];
     $kategori = $_POST['kategori'];
     $harga_jual = $_POST['harga_jual'];
@@ -11,19 +10,18 @@ if (isset($_POST['submit']))
     $stok = $_POST['stok'];
     $file_gambar = $_FILES['file_gambar'];
     $gambar = null;
-
-    if ($file_gambar['error'] == 0)
-    {
-        $filename = str_replace(' ', '_',$file_gambar['name']);
-        $destination = dirname(__FILE__) .'/gambar/' . $filename;
-        if(move_uploaded_file($file_gambar['tmp_name'], $destination))
-        {
+    if ($file_gambar['error'] == 0) {
+        $filename = str_replace(' ', '_', $file_gambar['name']);
+        $destination = dirname(__FILE__) . '/gambar/' . $filename;
+        if (move_uploaded_file($file_gambar['tmp_name'], $destination)) {
             $gambar = 'gambar/' . $filename;
+            ;
         }
     }
-    
-    $sql = 'INSERT INTO data_barang (nama, kategori, harga_jual, harga_beli, stok, gambar) ';
-    $sql .= "VALUE ('{$nama}', '{$kategori}','{$harga_jual}','{$harga_beli}', '{$stok}', '{$gambar}')";
+    $sql = 'INSERT INTO data_barang (nama, kategori,harga_jual, harga_beli,
+    stok, gambar) ';
+    $sql .= "VALUE ('{$nama}', '{$kategori}','{$harga_jual}',
+    '{$harga_beli}', '{$stok}', '{$gambar}')";
     $conn = mysqli_connect($host, $user, $pass, $db);
     $result = mysqli_query($conn, $sql);
     header('location: index.php');
@@ -32,49 +30,53 @@ if (isset($_POST['submit']))
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
-    <link href="style.css" rel="stylesheet" type="text/css" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Tambah Barang</title>
 </head>
+
 <body>
-<div class="container">
-    <h1>Tambah Barang</h1>
-    <div class="main">
-        <form method="post" action="tambah.php" enctype="multipart/form-data">
-            <div class="input">
-                <label>Nama Barang</label>
-                <input type="text" name="nama" />
-            </div>
-            <div class="input">
-                <label>Kategori</label>
-                <select name="kategori">
-                    <option value="Komputer">Komputer</option>
-                    <option value="Elektronik">Elektronik</option>
-                    <option value="Hand Phone">Hand Phone</option>
-                </select>
-            </div>
-            <div class="input">
-                <label>Harga Jual</label>
-                <input type="text" name="harga_jual" />
-            </div>
-            <div class="input">
-                <label>Harga Beli</label>
-                <input type="text" name="harga_beli"/>
-            </div>
-            <div class="input">
-                <label>Stok</label>
-                <input type="text" name="stok"/>
-            </div>
-            <div class="input">
-                <label>File Gambar</label>
-                <input type="file" name="file_gambar" />
-            </div>
-            <div class="submit">
-                <input type="submit" name="submit" value="Simpan" />
-            </div>
-        </form>
+    <div class="container">
+        <h1>Tambah Barang</h1>
+        <div class="main">
+            <form action="tambah.php" method="post" enctype="multipart/form-data">
+                <div class="input">
+                    <label>Nama Barang</label>
+                    <input type="text" name="nama" />
+                </div>
+                <div class="input">
+                    <label>Kategori</label>
+                    <select name="kategori">
+                        <option value="Komputer">Komputer</option>
+                        <option value="Elektronik">Elektronik</option>
+                        <option value="Handphone">Handphone</option>
+                    </select>
+                </div>
+                <div class="input">
+                    <label>Harga Jual</label>
+                    <input type="text" name="harga_jual" />
+                </div>
+                <div class="input">
+                    <label>Harga Beli</label>
+                    <input type="text" name="harga_beli" />
+                </div>
+                <div class="input">
+                    <label>Stok</label>
+                    <input type="text" name="stok" />
+                </div>
+                <div class="input">
+                    <label>File Gambar</label>
+                    <input type="file" name="file_gambar" />
+                </div>
+                <div class="submit">
+                    <input type="submit" name="submit" value="Simpan">
+                </div>
+            </form>
+        </div>
     </div>
-</div>
 </body>
+
 </html>
